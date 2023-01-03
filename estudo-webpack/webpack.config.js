a599ed66fd63f4bf5c45761cb848d972a7ce203a
Jean Paulino <jean.ssparaiso@gmail.com>
2023-01-02 22:22:32 -0300

    att

---
+++ estudo-webpack/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  devServer: {
+    static: "./",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /nodule_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
