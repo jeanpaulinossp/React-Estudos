cbe42cad3801c0048bbc74230d059ed8554ed41d
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-05 18:11:57 -0300

    ATT

--- react-estudos/src/App.js
+++ react-estudos/src/App.js
@@ -1,11 +1,13 @@
 import Exercicio01 from "./Exercicio01";
 import Exercicio02 from "./Exercicio02";
+import Exercicio03 from "./Exercicio03";
 
 const App = () => {
   return (
     <>
       {/* <Exercicio01 /> */}
-      <Exercicio02 />
+      {/* <Exercicio02 /> */}
+      {<Exercicio03 />}
     </>
   );
 };
