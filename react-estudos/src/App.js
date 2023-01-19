d57d8dffa621f9d43f50ec3a3ec9a49abd2c6751
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-01-19 15:57:59 -0300

    instalacao do react-router

--- react-estudos/src/App.js
+++ react-estudos/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Exercicio01 from "./Exercicio01";
 // import Exercicio02 from "./Exercicio02";
 // import Exercicio03 from "./Exercicio03";
@@ -10,40 +11,27 @@ import React from "react";
 // import Exercicio07 from "./Exercicio07";
 // import Exercicio08 from "./Exercicio08";
 // import Exercicio09 from "./Exercicio09";
-import Exercicio10 from "./Exercicio10";
+// import Exercicio10 from "./Exercicio10";
 
 const App = () => {
-  const slides = [
-    {
-      id: "slide1",
-      text: "Slide1",
-    },
-    {
-      id: "slide2",
-      text: "Slide2",
-    },
-    {
-      id: "slide3",
-      text: "Slide3",
-    },
-  ];
-
   return (
-    <>
-      {/* <Exercicio01 /> */}
-      {/* <Exercicio02 /> */}
-      {/* <Exercicio03 /> */}
-      {/* <Exercicio04 /> */}
-      {/* <Exercicio05 /> */}
-      {/* <Exercicio06Storage>
+    <BrowserRouter>
+      <Routes>
+        {/* <Exercicio01 /> */}
+        {/* <Exercicio02 /> */}
+        {/* <Exercicio03 /> */}
+        {/* <Exercicio04 /> */}
+        {/* <Exercicio05 /> */}
+        {/* <Exercicio06Storage>
         <Exercicio06 />
         <LimparEx06 />
       </Exercicio06Storage> */}
-      {/* <Exercicio07 /> */}
-      {/* <Exercicio08 /> */}
-      {/* <Exercicio09 /> */}
-      <Exercicio10 slides={slides} />
-    </>
+        {/* <Exercicio07 /> */}
+        {/* <Exercicio08 /> */}
+        {/* <Exercicio09 /> */}
+        {/* <Exercicio10 /> */}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
